3cbf59501bb5e74795598291e94682b89b245cdd
hama0115 <oh.akibee1231@gmail.com>
2025-03-22 04:10:48 +0900

    フロントページに最新の投稿と日記を取得して表示

---
+++ template-parts/excerpt-onlyheader.php
@@ -0,0 +1,15 @@
+<!-- 抜粋なし -->
+<article id="post-<?php the_ID(); //投稿IDを表示 ?>" <?php post_class(); //投稿のカテゴリーなどに基づきクラス属性を表示 ?>>
+	<header class="entry-header">
+		<?php the_title( '<h2 class="archive-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); //タイトルを表示 ?>
+	</header><!-- .entry-header -->
+
+	<figure class="post-thumbnail">
+		<?php the_post_thumbnail(); //アイキャッチ画像を表示 ?>
+	</figure><!-- .post-thumbnail -->
+
+	<footer class="entry-footer">
+		<span class="cat-links">カテゴリー: <?php the_category( ', ' ); //カテゴリーを表示 ?></span>
+		<?php the_tags( '<span class="tags-links">タグ: ', ', ', '</span>' ); //タグを表示 ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
