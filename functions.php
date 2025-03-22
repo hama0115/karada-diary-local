e436181d47d90814226a0c849992ed4b1e8e0757
hama0115 <oh.akibee1231@gmail.com>
2025-03-22 13:46:08 +0900

    フロントページ用のCSSを作成、functionsで読み込み

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-function sample_child_styles() {
+function sample_child_styles() { //子テーマのCSSを読み込む
 	wp_dequeue_style( 'sampletheme-style' );
 	wp_enqueue_style( 'sampletheme-parent-style', get_template_directory_uri() . '/style.css', [], wp_get_theme()->get( 'Version' ) );
 	wp_enqueue_style( 'sampletheme-child-style', get_stylesheet_uri(), ['sampletheme-parent-style'], wp_get_theme()->get( 'Version' ) );
@@ -10,6 +10,13 @@ function sample_child_setup() {
 	remove_filter( 'body_class', 'sampletheme_body_classes' );
 }
 add_action( 'after_setup_theme', 'sample_child_setup' );
+
+function enqueue_child_scripts() {
+	if( is_front_page()) {
+		wp_enqueue_style( 'front-page-style', get_stylesheet_directory_uri() . '/assets/css/front-page.css' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_child_scripts' );
 
 
 function sample_child_body_classes( $classes ) {
